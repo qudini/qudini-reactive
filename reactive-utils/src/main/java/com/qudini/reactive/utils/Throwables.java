6737634b5d867befbbb47e52c2bd711b95ba06b8
Christophe Maillard <christophe@qudini.com>
2020-03-01 19:03:22 +0000

    Starting reactive-utils as needed by reactive-logging

---
+++ reactive-utils/src/main/java/com/qudini/reactive/utils/Throwables.java
@@ -0,0 +1,136 @@
+package com.qudini.reactive.utils;
+
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
+public final class Throwables {
+
+    @FunctionalInterface
+    public interface ThrowableConsumer<I> extends Consumer<I> {
+
+        @Override
+        @SneakyThrows
+        default void accept(I i) {
+            acceptOrThrow(i);
+        }
+
+        void acceptOrThrow(I i) throws Throwable;
+
+    }
+
+    @FunctionalInterface
+    public interface ThrowableFunction<I, O> extends Function<I, O> {
+
+        @Override
+        @SneakyThrows
+        default O apply(I i) {
+            return applyOrThrow(i);
+        }
+
+        O applyOrThrow(I i) throws Throwable;
+
+    }
+
+    @FunctionalInterface
+    public interface ThrowableSupplier<O> extends Supplier<O> {
+
+        @Override
+        @SneakyThrows
+        default O get() {
+            return getOrThrow();
+        }
+
+        O getOrThrow() throws Throwable;
+
+    }
+
+    @FunctionalInterface
+    public interface ThrowablePredicate<I> extends Predicate<I> {
+
+        @Override
+        @SneakyThrows
+        default boolean test(I i) {
+            return testOrThrow(i);
+        }
+
+        boolean testOrThrow(I i) throws Throwable;
+    }
+
+    @FunctionalInterface
+    public interface ThrowableBiConsumer<I, J> extends BiConsumer<I, J> {
+
+        @Override
+        @SneakyThrows
+        default void accept(I i, J j) {
+            acceptOrThrow(i, j);
+        }
+
+        void acceptOrThrow(I i, J j) throws Throwable;
+
+    }
+
+    @FunctionalInterface
+    public interface ThrowableBiFunction<I, J, O> extends BiFunction<I, J, O> {
+
+        @Override
+        @SneakyThrows
+        default O apply(I i, J j) {
+            return applyOrThrow(i, j);
+        }
+
+        O applyOrThrow(I i, J j) throws Throwable;
+
+    }
+
+    @FunctionalInterface
+    public interface ThrowableBiPredicate<I, J> extends BiPredicate<I, J> {
+
+        @Override
+        @SneakyThrows
+        default boolean test(I i, J j) {
+            return testOrThrow(i, j);
+        }
+
+        boolean testOrThrow(I i, J j) throws Throwable;
+    }
+
+    public static <I> Consumer<I> consumer(ThrowableConsumer<I> consumer) {
+        return consumer;
+    }
+
+    public static <I, O> Function<I, O> function(ThrowableFunction<I, O> function) {
+        return function;
+    }
+
+    public static <O> Supplier<O> supplier(ThrowableSupplier<O> supplier) {
+        return supplier;
+    }
+
+    public static <I> Predicate<I> predicate(ThrowablePredicate<I> predicate) {
+        return predicate;
+    }
+
+    public static <I, J> BiConsumer<I, J> biConsumer(ThrowableBiConsumer<I, J> consumer) {
+        return consumer;
+    }
+
+    public static <I, J, O> BiFunction<I, J, O> biFunction(ThrowableBiFunction<I, J, O> function) {
+        return function;
+    }
+
+    public static <I, J> BiPredicate<I, J> biPredicate(ThrowableBiPredicate<I, J> predicate) {
+        return predicate;
+    }
+
+}
---
+++ reactive-utils/src/test/java/com/qudini/reactive/utils/ThrowablesTest.java
@@ -0,0 +1,187 @@
+package com.qudini.reactive.utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+import static com.qudini.reactive.utils.Throwables.biConsumer;
+import static com.qudini.reactive.utils.Throwables.biFunction;
+import static com.qudini.reactive.utils.Throwables.biPredicate;
+import static com.qudini.reactive.utils.Throwables.consumer;
+import static com.qudini.reactive.utils.Throwables.function;
+import static com.qudini.reactive.utils.Throwables.predicate;
+import static com.qudini.reactive.utils.Throwables.supplier;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DisplayName("Throwables")
+class ThrowablesTest {
+
+    @Test
+    @DisplayName("should handle successful throwable consumer")
+    void consumerSuccess() {
+        takeConsumer(consumer(i -> {
+        }));
+    }
+
+    @Test
+    @DisplayName("should handle erroneous throwable consumer")
+    void consumerError() {
+        assertThrows(
+                TestException.class,
+                () -> takeConsumer(consumer(i -> {
+                    throw new TestException();
+                }))
+        );
+    }
+
+    @Test
+    @DisplayName("should handle successful throwable function")
+    void functionSuccess() {
+        var output = takeFunction(function(i -> 42));
+        assertThat(output).isEqualTo(42);
+    }
+
+    @Test
+    @DisplayName("should handle erroneous throwable function")
+    void functionError() {
+        assertThrows(
+                TestException.class,
+                () -> takeFunction(function(i -> {
+                    throw new TestException();
+                }))
+        );
+    }
+
+    @Test
+    @DisplayName("should handle successful throwable supplier")
+    void supplierSuccess() {
+        var output = takeSupplier(supplier(() -> 42));
+        assertThat(output).isEqualTo(42);
+    }
+
+    @Test
+    @DisplayName("should handle erroneous throwable supplier")
+    void supplierError() {
+        assertThrows(
+                TestException.class,
+                () -> takeSupplier(supplier(() -> {
+                    throw new TestException();
+                }))
+        );
+    }
+
+    @Test
+    @DisplayName("should handle successful throwable predicate")
+    void predicateSuccess() {
+        var output = takePredicate(predicate(i -> true));
+        assertThat(output).isTrue();
+    }
+
+    @Test
+    @DisplayName("should handle erroneous throwable predicate")
+    void predicateError() {
+        assertThrows(
+                TestException.class,
+                () -> takePredicate(predicate(i -> {
+                    throw new TestException();
+                }))
+        );
+    }
+
+    @Test
+    @DisplayName("should handle successful throwable biconsumer")
+    void biConsumerSuccess() {
+        takeBiConsumer(biConsumer((i, j) -> {
+        }));
+    }
+
+    @Test
+    @DisplayName("should handle erroneous throwable biconsumer")
+    void biConsumerError() {
+        assertThrows(
+                TestException.class,
+                () -> takeBiConsumer(biConsumer((i, j) -> {
+                    throw new TestException();
+                }))
+        );
+    }
+
+    @Test
+    @DisplayName("should handle successful throwable bifunction")
+    void biFunctionSuccess() {
+        var output = takeBiFunction(biFunction((i, j) -> 42));
+        assertThat(output).isEqualTo(42);
+    }
+
+    @Test
+    @DisplayName("should handle erroneous throwable bifunction")
+    void biFunctionError() {
+        assertThrows(
+                TestException.class,
+                () -> takeBiFunction(biFunction((i, j) -> {
+                    throw new TestException();
+                }))
+        );
+    }
+
+    @Test
+    @DisplayName("should handle successful throwable bipredicate")
+    void biPredicateSuccess() {
+        var output = takeBiPredicate(biPredicate((i, j) -> true));
+        assertThat(output).isTrue();
+    }
+
+    @Test
+    @DisplayName("should handle erroneous throwable bipredicate")
+    void biPredicateError() {
+        assertThrows(
+                TestException.class,
+                () -> takeBiPredicate(biPredicate((i, j) -> {
+                    throw new TestException();
+                }))
+        );
+    }
+
+    private void takeConsumer(Consumer<Integer> consumer) {
+        consumer.accept(42);
+    }
+
+    private Integer takeFunction(Function<Integer, Integer> function) {
+        return function.apply(42);
+    }
+
+    private Integer takeSupplier(Supplier<Integer> supplier) {
+        return supplier.get();
+    }
+
+    private boolean takePredicate(Predicate<Integer> predicate) {
+        return predicate.test(42);
+    }
+
+    private void takeBiConsumer(BiConsumer<Integer, Integer> consumer) {
+        consumer.accept(42, 42);
+    }
+
+    private Integer takeBiFunction(BiFunction<Integer, Integer, Integer> function) {
+        return function.apply(42, 42);
+    }
+
+    private boolean takeBiPredicate(BiPredicate<Integer, Integer> predicate) {
+        return predicate.test(42, 42);
+    }
+
+    private static final class TestException extends Exception {
+
+        private static final long serialVersionUID = 1L;
+
+    }
+
+}
