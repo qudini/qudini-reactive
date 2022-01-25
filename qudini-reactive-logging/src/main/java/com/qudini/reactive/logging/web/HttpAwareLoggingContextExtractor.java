1d78a23f079bc5f07e15c7999a16fbf35aee7f24
Christophe Maillard <christophe@qudini.com>
2022-01-25 18:29:24 +0100

    Improving code

--- qudini-reactive-logging/src/main/java/com/qudini/reactive/logging/web/HttpAwareLoggingContextExtractor.java
+++ qudini-reactive-logging/src/main/java/com/qudini/reactive/logging/web/HttpAwareLoggingContextExtractor.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import reactor.core.publisher.Mono;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -12,10 +11,10 @@ public final class HttpAwareLoggingContextExtractor implements LoggingContextExt
 
     @Override
     public Mono<Map<String, String>> extract(ServerHttpRequest request) {
-        Map<String, String> context = new HashMap<>();
-        context.put("request", request.getMethodValue() + " " + request.getPath().pathWithinApplication().value());
-        context.put("user_agent", String.join(", ", request.getHeaders().getOrEmpty("User-Agent")));
-        return Mono.just(Map.copyOf(context));
+        return Mono.just(Map.of(
+                "request", request.getMethodValue() + " " + request.getPath().pathWithinApplication().value(),
+                "user_agent", String.join(", ", request.getHeaders().getOrEmpty("User-Agent"))
+        ));
     }
 
 }
